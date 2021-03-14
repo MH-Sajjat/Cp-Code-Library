9fbb1f397c3b3e3a25bc20c10b4813ea114ff838
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2021-03-14 08:50:14 +0600

    dp

---
+++ dynamic programming/lcs/lcs_recursive.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef  long long               ll;
+typedef  long double             ld;
+typedef  unsigned long long      ull;
+typedef  pair<int, int>          pii;
+typedef  pair<ll, ll>            pll;
+typedef  vector<int>             vi;
+typedef  vector<ll>              vll;
+typedef  tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_set;
+typedef  tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_multiset;
+
+inline int Int() { int x; scanf("%d", &x); return x; }
+inline ll Long() { ll x; scanf("%lld", &x); return x; }
+
+int dx[8] = { 0, -1, 0, 1, -1, -1, 1, 1 };
+int dy[8] = { -1, 0, 1, 0, -1, 1, 1, -1 };
+
+const int N       = (int) 1e3 + 5;
+const int mxN     = (int) 1e6 + 6;
+const ll MOD      = (ll) 1e9 + 7;
+
+#define    debug(x)    cerr << #x << " = " << x << '\n';
+#define    all(x)      x.begin(), x.end()
+#define    szof(x)     (int) x.size()
+#define    ff          first
+#define    ss          second
+#define    pb          push_back
+#define    mp          make_pair
+#define    PI          acos(-1.0)
+#define    TN          typename
+#define    nl          '\n'
+#define    Fast_IO     ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+template <TN T> T gcd(T a, T b) { return !b ? a : gcd(b, a % b); }
+template <TN T> T lcm(T a, T b) { return a * (b / gcd(a, b)); }
+
+string s, t;
+
+vector<vi> dp;
+vector<vector<pii>> par;
+
+int fun(int n, int m) {
+	if (n == -1 || m == -1) return 0;
+	if (dp[n][m] != -1) return dp[n][m];
+	if (s[n] == t[m]) {
+		par[n][m] = {n - 1, m - 1};
+		return dp[n][m] = 1 + fun(n - 1, m - 1);
+	}
+	int u = fun(n, m - 1);
+	int v = fun(n - 1, m);
+	if (u > v) {
+		par[n][m] = {n, m - 1};
+		return dp[n][m] = u;
+	} else {
+		par[n][m] = {n - 1, m};
+		return dp[n][m] = v;
+	}
+}
+
+void path(int n, int m) {
+	if (n == -1 || m == -1) return;
+	path(par[n][m].ff, par[n][m].ss);
+	if (s[n] == t[m]) printf("%c", s[n]);
+}
+
+int main() {
+    //Fast_IO
+    //clock_t tStart = clock();
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int test = 1, tc = 0;
+    while (test--) {
+        dp.resize(N + 1, vi(N + 1, -1));
+        par.resize(N + 1, vector<pii>(N + 1, mp(-1, -1)));
+
+        cin >> s >> t;
+        int n = szof(s) - 1, m = szof(t) - 1;
+        printf("%d\n", fun(n, m));
+        path(n, m);
+        printf("\n");
+    }
+    //fprintf(stderr, "\nRuntime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    return 0;
+}
