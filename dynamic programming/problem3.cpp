dec30e0a3b6559ff3b7f082948d3ec0eda1a7388
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2021-03-15 22:01:36 +0600

    dp

---
+++ dynamic programming/problem3.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef  long long               ll;
+typedef  long double             ld;
+typedef  unsigned long long      ull;
+typedef  pair<int, int>          pii;
+typedef  pair<ll, ll>            pll;
+typedef  vector<int>             vi;
+typedef  vector<ll>              vll;
+typedef  tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_set;
+typedef  tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_multiset;
+
+inline int Int() { int x; scanf("%d", &x); return x; }
+inline ll Long() { ll x; scanf("%lld", &x); return x; }
+
+int dx[8] = { 0, -1, 0, 1, -1, -1, 1, 1 };
+int dy[8] = { -1, 0, 1, 0, -1, 1, 1, -1 };
+
+const int N       = (int) 2e5 + 5;
+const int mxN     = (int) 1e6 + 6;
+const ll MOD      = (ll) 1e9 + 7;
+
+#define    debug(x)    cerr << #x << " = " << x << '\n';
+#define    all(x)      x.begin(), x.end()
+#define    szof(x)     (int) x.size()
+#define    ff          first
+#define    ss          second
+#define    pb          push_back
+#define    mp          make_pair
+#define    PI          acos(-1.0)
+#define    TN          typename
+#define    nl          '\n'
+#define    Fast_IO     ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+template <TN T> T gcd(T a, T b) { return !b ? a : gcd(b, a % b); }
+template <TN T> T lcm(T a, T b) { return a * (b / gcd(a, b)); }
+
+// Given an array with N integers. find a subsequence of this array to maximize the sum of subsequence.
+// condition - can't take two adjacent element number
+
+int ar[N], n;
+int dp[N][2];
+
+// complexity O(n)
+int fun(int i, int pre) {
+	if (i > n) return 0;
+	if (dp[i][pre] != -1) return dp[i][pre];
+	if (pre == 0) {
+		return dp[i][pre] = max((ar[i] + fun(i + 1, 1)), fun(i + 1, 0));
+	} else {
+		return dp[i][pre] = fun(i + 1, 0);
+	}
+}
+
+int main() {
+    //Fast_IO
+    //clock_t tStart = clock();
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int test = 1, tc = 0;
+    while (test--) {
+    	memset(dp, -1, sizeof dp);
+       	n = Int();
+        for (int i = 1; i <= n; ++i) {
+        	ar[i] = Int();
+        }
+        printf("%d\n", fun(1, 0));
+    }
+    //fprintf(stderr, "\nRuntime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    return 0;
+}
