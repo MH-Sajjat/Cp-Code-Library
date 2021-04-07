aa13e1d1680f40de70b7022ea4c07fced23fa4b4
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2021-04-07 08:19:48 +0600

    dp

---
+++ dynamic programming/dp with bitmask/problem_2.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef  long long               ll;
+typedef  long double             ld;
+typedef  unsigned long long      ull;
+typedef  pair<int, int>          pii;
+typedef  pair<ll, ll>            pll;
+typedef  vector<int>             vi;
+typedef  vector<ll>              vll;
+typedef  tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_set;
+typedef  tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_multiset;
+
+inline int Int() { int x; scanf("%d", &x); return x; }
+inline ll Long() { ll x; scanf("%lld", &x); return x; }
+
+int dx[8] = { 0, -1, 0, 1, -1, -1, 1, 1 };
+int dy[8] = { -1, 0, 1, 0, -1, 1, 1, -1 };
+
+const int N       = (int) 2e5 + 5;
+const int mxN     = (int) 1e6 + 6;
+const int MOD     = (ll) 1e9 + 7;
+
+#define    debug(x)    cerr << #x << " = " << x << '\n';
+#define    all(x)      x.begin(), x.end()
+#define    szof(x)     (int) x.size()
+#define    ff          first
+#define    ss          second
+#define    pb          push_back
+#define    mp          make_pair
+#define    PI          acos(-1.0)
+#define    TN          typename
+#define    nl          '\n'
+#define    Fast_IO     ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+template <TN T> T gcd(T a, T b) { return !b ? a : gcd(b, a % b); }
+template <TN T> T lcm(T a, T b) { return a * (b / gcd(a, b)); }
+
+// Given a string S(only digit) and an integer D. How many permutation of S is divisible by D.
+// S <= 10, 1 <= D <= 1000 and all the digit of S are unique;
+
+string s;
+int dp[(1 << 10) + 5][1050];
+int d;
+
+int fun(int mask, int m) {
+	int pos = __builtin_popcount(mask);
+	if (pos == szof(s)) return m == 0;
+	int& res = dp[mask][m];
+	if (res != -1) return res;
+	res = 0;
+	for (int i = 0; i < szof(s); ++i) {
+		if (mask & (1 << i)) continue;
+		res += fun(mask | (1 << i), (m * 10 + s[i] - '0') % d);
+	}
+	return res;
+}
+
+int main() {
+    Fast_IO
+    // clock_t tStart = clock();
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int test = 1, tc = 0;
+    while (test--) {
+    	memset(dp, -1, sizeof dp);
+        cin >> s >> d;
+        cout << fun(0, 0) << nl;
+    }
+    // fprintf(stderr, "\nRuntime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    return 0;
+}
