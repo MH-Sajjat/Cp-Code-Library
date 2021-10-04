03904f0ab098dee6f7d8cf6e1875cff3a4da0d8b
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2021-10-04 07:51:36 +0600

    Update class_seg_tree_lazy_sum.cpp

--- data structure/segment tree/class_seg_tree_lazy_sum.cpp
+++ data structure/segment tree/class_seg_tree_lazy_sum.cpp
@@ -1,3 +1,9 @@
+/*---------------------------------
+bismillahir rahmanir rahim
+@author : md. sajjat hossen
+created : 04-October,2021 07:02:48
+----------------------------------*/
+
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -5,15 +11,15 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-typedef  long long               ll;
-typedef  long double             ld;
-typedef  unsigned long long      ull;
-typedef  pair<int, int>          pii;
-typedef  pair<ll, ll>            pll;
-typedef  vector<int>             vi;
-typedef  vector<ll>              vll;
-typedef  tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_set;
-typedef  tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_multiset;
+typedef long long          ll;
+typedef long double        ld;
+typedef unsigned long long ull;
+typedef pair <int, int>    pii;
+typedef pair <ll, ll>      pll;
+typedef vector <int>       vi;
+typedef vector <ll>        vll;
+typedef tree <int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_set;
+typedef tree <int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_multiset;
 
 inline int Int() { int x; scanf("%d", &x); return x; }
 inline ll Long() { ll x; scanf("%lld", &x); return x; }
@@ -21,119 +27,131 @@ inline ll Long() { ll x; scanf("%lld", &x); return x; }
 int dx[8] = { 0, -1, 0, 1, -1, -1, 1, 1 };
 int dy[8] = { -1, 0, 1, 0, -1, 1, 1, -1 };
 
-const int N       = (int) 2e5 + 5;
-const int mxN     = (int) 1e6 + 6;
-const int MOD     = (int) 1e9 + 7;
+const int N      = (int) 2e5 + 5;
+const int mxN    = (int) 1e6 + 6;
+const int MOD    = (int) 1e9 + 7;
+const int INF    = (int) 1e9 + 9;
+const double EPS = (double) 1e-9;
 
-#define    debug(x)    cerr << #x << " = " << x << '\n';
-#define    all(x)      x.begin(), x.end()
-#define    szof(x)     (int) x.size()
-#define    ff          first
-#define    ss          second
-#define    pb          push_back
-#define    mp          make_pair
-#define    pie         acos(-1.0)
-#define    TN          typename
-#define    nl          '\n'
-#define    Fast_IO     ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define debug(x) cerr << #x << " = " << x << '\n';
+#define all(x)   x.begin(), x.end()
+#define szof(x)  (int) x.size()
+#define ff       first
+#define ss       second
+#define pb       push_back
+#define mp       make_pair
+#define PI       acos(-1.0)
+#define TN       typename
+#define nl       '\n'
+#define Fast_IO  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+inline int add(int a, int b, int mod) { a += b; return a >= mod ? a - mod : a < 0 ? a + mod : a; }
+inline int mult(int a, int b, int mod) { return (ll) a * b % mod; }
+
+template <TN T> inline void umin(T &x, T y) { x = x < y ? x : y; }
+template <TN T> inline void umax(T &x, T y) { x = x > y ? x : y; }
 
 template <TN T> T gcd(T a, T b) { return !b ? a : gcd(b, a % b); }
 template <TN T> T lcm(T a, T b) { return a * (b / gcd(a, b)); }
 
-int ar[N];
+int nums[N];
 
-// complexity O(nlogn)
-template<TN T> class SegTree{
+template <typename T> class SegTree {
 private:
-    struct data{
-        T sum, prop;
-        data() { sum = prop = 0; }
+    struct data {
+        T sum, lazy;
+        data() : sum(0), lazy(0) {};
     };
-    vector<data> g;
+
     int n;
+    vector<data> tree;
 
-    // complexity O(n)
-    void build(int at, int l, int r) {
-        if (l == r) {
-            g[at].sum = ar[l];
+    void build(int at, int b, int e) {
+        if (b == e) {
+            tree[at].sum = nums[b];
             return;
         }
-        int mid = (l + r) >> 1;
-        build(2 * at, l, mid);
-        build(2 * at + 1, mid + 1, r);
-        g[at].sum = g[2 * at].sum + g[2 * at + 1].sum;
+        int mid = (b + e) >> 1;
+        build(2 * at, b, mid);
+        build(2 * at + 1, mid + 1, e);
+        tree[at].sum = tree[2 * at].sum + tree[2 * at + 1].sum;
     }
 
-    void propagate(int at, int l, int r) {
-        int mid = (l + r) >> 1;
-        g[2 * at].sum += (g[at].prop * (mid - l + 1));
-        g[2 * at + 1].sum += (g[at].prop * (r - mid));
-        g[2 * at].prop += g[at].prop;
-        g[2 * at + 1].prop += g[at].prop;
-        g[at].prop = 0;
+    void propagate(int at, int b, int e) {
+        tree[at].sum += (e - b + 1) * tree[at].lazy;
+        if (b != e) {
+            tree[2 * at].lazy += tree[at].lazy;
+            tree[2 * at + 1].lazy += tree[at].lazy;
+        }
+        tree[at].lazy = 0;
     }
 
-    // complexity O(logn)
-    void update(int at, int l, int r, int L, int R, int val) {
-        if (R < l || r < L) return;
-        if (L <= l && r <= R) {
-            g[at].sum += (val * (r - l + 1));
-            g[at].prop += val;
+    void update(int at, int b, int e, int l, int r, int val) {
+        if (tree[at].lazy) propagate(at, b, e);
+        if (r < b or e < l) return;
+        if (l <= b and e <= r) {
+            tree[at].sum += (e - b + 1) * val;
+            if (b != e) {
+                tree[2 * at].lazy += val;
+                tree[2 * at + 1].lazy += val;
+            }
             return;
         }
-        if (g[at].prop) propagate(at, l, r);
-        int mid = (l + r) >> 1;
-        update(2 * at, l, mid, L, R, val);
-        update(2 * at + 1, mid + 1, r, L, R, val);
-        g[at].sum = g[2 * at].sum + g[2 * at + 1].sum;
+        int mid = (b + e) >> 1;
+        update(2 * at, b, mid, l, r, val);
+        update(2 * at + 1, mid + 1, e, l, r, val);
+        tree[at].sum = tree[2 * at].sum + tree[2 * at + 1].sum;
     }
 
-    // complexity O(logn)
-    T query(int at, int l, int r, int L, int R) {
-        if (R < l || r < L) return 0;
-        if (L <= l && r <= R) return g[at].sum;
-        if (g[at].prop) propagate(at, l, r);
-        int mid = (l + r) >> 1;
-        int u = query(2 * at, l, mid, L, R);
-        int v = query(2 * at + 1, mid + 1, r, L, R);
-        return u + v;
+    T query(int at, int b, int e, int l, int r) {
+        if (tree[at].lazy) propagate(at, b, e);
+        if (r < b or e < l) return 0;
+        if (l <= b and e <= r) return tree[at].sum;
+        int mid = (b + e) >> 1;
+        int x = query(2 * at, b, mid, l, r);
+        int y = query(2 * at + 1, mid + 1, e, l, r);
+        return x + y;
     }
 
 public:
-    SegTree(int _n) {
-        g.resize(4 * N);
-        n = _n;
+    SegTree(int n) {
+        this->n = n;
+        tree.resize(4 * N);
         build(1, 1, n);
     }
-    
-    void update(int l, int r, int val) { update(1, 1, n, l, r, val); }
-    T query(int l, int r) { return query(1, 1, n, l, r); }
+
+    void update(int l, int r, int val) {
+        update(1, 1, n, l, r, val);
+    }
+
+    T query(int l, int r) {
+        return query(1, 1, n, l, r);
+    }
 };
 
 int main() {
-    //Fast_IO
-    //clock_t tStart = clock();
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif
+    // Fast_IO
     int test = 1, tc = 0;
     while (test--) {
-        int n = Int(), q = Int();
-        for (int i = 1; i <= n; ++i) {
-            ar[i] = Int();
-        }
+        int n = Int();
+        for (int i = 1; i <= n; ++i) nums[i] = Int();
+
         SegTree<int> ds(n);
+        int q = Int();
         while (q--) {
-            int ck = Int();
-            if (ck == 1) {
+            int t = Int();
+            if (t == 0) {
                 int l = Int(), r = Int(), val = Int();
                 ds.update(l, r, val);
             } else {
                 int l = Int(), r = Int();
-                int res = ds.query(l, r);
-                printf("%d\n", res);
+                printf("%d\n", ds.query(l, r));
             }
         }
     }
-    //fprintf(stderr, "\nRuntime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
     return 0;
 }
